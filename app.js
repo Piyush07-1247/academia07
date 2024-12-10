8beaac2a6e8e02eb7c19159f039d8954649578a9
Piyush07-1247 <gangwarpiyush827@gmail.com>
2024-12-10 22:07:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ function navigateTo(page) {
   switch (page) {
     case 'login':
       alert("Redirecting to Login...");
-       window.location.href = 'file:///F:/DOWNLOADS/PROJECT/login.html'; // Ensure page exists
+       window.location.href = 'login.html'; // Ensure page exists
       break;
     case 'register':
       alert("Redirecting to Register...");
-      window.location.href = 'file:///F:/DOWNLOADS/PROJECT/register.html'; // Ensure page exists
+      window.location.href = 'register.html'; // Ensure page exists
       break;
     case 'attendance':
-      window.location.href = 'file:///F:/DOWNLOADS/PROJECT/attendance/attend.html'; // Ensure page exists
+      window.location.href = 'attend.html'; // Ensure page exists
       break;
     case 'marks':
-      window.location.href = 'file:///F:/DOWNLOADS/PROJECT/internal/internal.html'; // Ensure page exists
+      window.location.href = 'internal.html'; // Ensure page exists
       break;
     case 'cgpa':
-      window.location.href = 'file:///F:/DOWNLOADS/PROJECT/cgpa/cgpa.html'; // Ensure page exists
+      window.location.href = 'cgpa.html'; // Ensure page exists
       break;
     default:
       alert('Coming Soon!');
